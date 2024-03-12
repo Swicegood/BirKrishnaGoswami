a65cd56952c21ce46190f96878ab273cc628cd5f
Swicegood <jguru108@gmail.com>
2024-03-12 19:32:06 -0400

    Add lables to Temples

--- app/TemplesScreen.tsx
+++ app/TemplesScreen.tsx
@@ -1,26 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import { Dimensions, FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { Dimensions, FlatList, Image, TouchableOpacity, StyleSheet, Text } from 'react-native';
 import { Link } from 'expo-router';
 import { getAllTempleFiles } from './api/apiWrapper';
 import placeholderImage from '../assets/images/placeholder-podq8jasdkjc0jdfrw96hbgsm3dx9f5s9dtnqlglf4.png'; // replace with your placeholder image path
-
+import { collection, getFirestore, query, orderBy, limit, getDocs, where, addDoc } from "firebase/firestore";
 
   
 const screenWidth = Dimensions.get('window').width;
 
 
 const GalleryComponent = () => {
+
+  type ImageItem = {
+    url: string;
+    description: string;
+    location: string;
+  };
+  
+  const [images, setImages] = useState<ImageItem[]>([]);
   const [initialImage] = useState(0);
 
-const [images, setImages] = useState<string[]>([]);
   useEffect(() => {
-    getAllTempleFiles().then((data) => {
-      setImages(data);
-    });
-  }, []);
-
+    const db = getFirestore();
+    const newImages = [];
+    const q = query(collection(db, 'temples'), orderBy('location'));
   
-
+    getDocs(q)
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          const data = doc.data();
+          newImages.push({
+            url: data.url,
+            description: data.description,
+            location: data.location,
+            date: data.date,
+          });
+        });
+        setImages(newImages);
+      })
+      .catch((error) => console.log('Error getting documents: ', error));
+  }, []);
+  
   return (
     <FlatList
       data={images}
@@ -28,13 +48,29 @@ const [images, setImages] = useState<string[]>([]);
       keyExtractor={(item, index) => index.toString()}
       renderItem={({ item, index }) => (
         <Link href={ {pathname: "./GalleryScreen", params: {imageChunk: item}} }asChild>
-        <TouchableOpacity>
-          <Image defaultSource={placeholderImage} source={{uri: item}} style={{ width: screenWidth / 2, height: 200}} />
+        <TouchableOpacity style={{ padding: 20, borderRadius: 10 }}>
+          <Image defaultSource={placeholderImage} source={{uri: item.url}} style={{ width: (screenWidth / 2) - 40, height: 170, borderRadius: 10 }} />  
+          <Text style={styles.text} >{item.location}</Text>
+          <Text style={[styles.text, { flexShrink: 1 }]}>{item.description}</Text>
         </TouchableOpacity>
         </Link>
       )}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  
+});
 
 export default GalleryComponent;
