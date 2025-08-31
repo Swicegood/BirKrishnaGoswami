1146c744501a3863e14e036b3cb781e5cf457f72
Swicegood <jguru108@gmail.com>
2025-08-31 13:52:25 -0400

    Update app configuration and dependencies: Increment build number to 23, add audio background mode, and disable non-exempt encryption. Upgrade 'react-native-share' to version 12.2.0 and update TypeScript and React types to their latest versions for improved compatibility. Refactor metro.config.js for cleaner configuration.

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,5 @@
-
-// Metro is a tool that bundles JavaScript code. It takes in options, and returns a bundler function.
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
-});
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
