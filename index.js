e0af713f42b1fe42383fd53a25a0af18fb15acbd
Swicegood <jguru108@gmail.com>
2025-09-15 15:50:51 -0400

    Make service registration resilient to logger failures
    - Move logger import inside try-catch to prevent blocking service registration
    - Service registration will now run even if logger fails
    - This should help identify if logger import is the issue

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { registerRootComponent } from 'expo';
 import TrackPlayer from 'react-native-track-player';
-import logger from './utils/logger';
 
 import App from './App';
 
@@ -10,16 +9,29 @@ import App from './App';
 registerRootComponent(App);
 
 // Register TrackPlayer service AFTER registerRootComponent (like BKGAudio)
-logger.info('INDEX.JS: About to register TrackPlayer service', {}, 'Index');
+// Try to use logger, but don't let it block execution if it fails
+let logger = null;
+try {
+  logger = require('./utils/logger').default;
+  logger.info('INDEX.JS: About to register TrackPlayer service', {}, 'Index');
+} catch (loggerError) {
+  // Logger failed, but continue with service registration
+}
 
 try {
   TrackPlayer.registerPlaybackService(() => {
-    logger.info('INDEX.JS: TrackPlayer service function called', {}, 'Index');
+    if (logger) {
+      logger.info('INDEX.JS: TrackPlayer service function called', {}, 'Index');
+    }
     return require('./service');
   });
-  logger.info('INDEX.JS: TrackPlayer service registered successfully', {}, 'Index');
+  if (logger) {
+    logger.info('INDEX.JS: TrackPlayer service registered successfully', {}, 'Index');
+  }
 } catch (error) {
-  logger.error('INDEX.JS: Failed to register TrackPlayer service', { 
-    error: error instanceof Error ? error.message : String(error) 
-  }, 'Index');
+  if (logger) {
+    logger.error('INDEX.JS: Failed to register TrackPlayer service', { 
+      error: error instanceof Error ? error.message : String(error) 
+    }, 'Index');
+  }
 }
